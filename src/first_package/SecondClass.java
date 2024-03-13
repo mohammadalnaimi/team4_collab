217ae1da7882d3b1e360e74371ebe496fdfce1cb
Aisulu <aisrysbekv@gmail.com>
2024-03-12 20:58:52 -0500

    Created SecondClass

---
+++ src/first_package/SecondClass.java
@@ -0,0 +1,11 @@
+package first_package;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello guys!");
+
+        System.out.println("How are you?");
+    }
+}
