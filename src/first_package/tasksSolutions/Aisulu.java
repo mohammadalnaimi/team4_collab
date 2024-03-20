69053a51080ea470238f2c2d25e7eb8c4bf6212d
Aisulu <aisrysbekv@gmail.com>
2024-03-20 12:22:23 -0500

    Created separate package for solutions and posted my solution in Aisulu class

---
+++ src/first_package/tasksSolutions/Aisulu.java
@@ -0,0 +1,28 @@
+package first_package.tasksSolutions;
+
+public class Aisulu {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        int factorial = 1;
+
+        for (int i = 1; i <= num ; i++) {
+                factorial*=i;
+        }
+        System.out.println(factorial);
+
+    }
+}
+/*
+Task for Amel to do ASAP
+5. Create a class named FactorialNumber. Write a program that can return the factorial number of any given number.
+
+				Example:
+					  num = 5
+
+				Output:
+					  120
+
+			(because: 5! = 5 * 4 * 3 * 2* 1 = 120 ) */
