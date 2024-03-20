7ba2e093aa0ef1651656cbc6d52248c500c54b10
mohammadalnaimi <alnaimijo88@gmail.com>
2024-03-19 23:49:44 -0500

    second commit

---
+++ src/first_package/TaskForNumbersFactory.java
@@ -0,0 +1,16 @@
+package first_package;
+
+public class TaskForNumbersFactory {
+}
+/*
+Task for Amel to do ASAP
+5. Create a class named FactorialNumber. Write a program that can return the factorial number of any given number.
+
+				Example:
+					  num = 5
+
+				Output:
+					  120
+
+			(because: 5! = 5 * 4 * 3 * 2* 1 = 120 )
+ */
