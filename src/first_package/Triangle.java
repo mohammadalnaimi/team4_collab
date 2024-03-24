fdaa167f275a2178b6af5c2ace12f37cbce2dbde
Adam <06adam.sal@gmail.com>
2024-03-24 15:15:31 -0500

    Amel's tasks complete

---
+++ src/first_package/Triangle.java
@@ -0,0 +1,23 @@
+package first_package;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+ /*
+ 7. Create a class named Triangle and use a nested loop to display the following shape on the console:
+
+
+  */
